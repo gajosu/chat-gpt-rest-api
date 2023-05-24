b13f629cede1348589846ce6ac4c79db7b060613
Gabriel Gonzalez <gajosu15@gmail.com>
2023-05-24 23:42:21 +0000

    add access token checker middleware

---
+++ chatgpt/middleware/access_token.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from typing import Optional
+
+class AccessTokenMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request)-> Optional[JsonResponse]:
+        access_token: Optional[str] = request.headers.get("Authorization", None)
+        if access_token is None:
+            #json response
+            return JsonResponse({
+                "error": "access_token is required"
+            }, status=401)
+
+        return self.get_response(request)

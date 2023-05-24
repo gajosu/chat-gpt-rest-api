c79071aafb4bbd222b5d40b2570f837cc734752c
Gabriel Gonzalez <gajosu15@gmail.com>
2023-05-24 23:42:26 +0000

    add error handler

---
+++ chatgpt/middleware/error_handler.py
@@ -0,0 +1,34 @@
+from OpenAIAuth import Error as AuthError
+from revChatGPT.typings import ChatbotError
+from django.http import JsonResponse
+from typing import Optional
+import json
+
+class ErrorHandlerMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_exception(self, request, exception) -> Optional[JsonResponse]:
+        
+        if isinstance(exception, AuthError):
+            return JsonResponse({
+                "error": {
+                    "location": exception.location,
+                    "status_code": exception.status_code,
+                    "details": exception.details
+                }
+            }, status=500)
+            
+            
+        if isinstance(exception, ChatbotError):
+            # parse exception.message json and return it
+            jsonMessage = json.loads(exception.message)
+            
+            return JsonResponse({
+                "error": jsonMessage
+            }, status=500)

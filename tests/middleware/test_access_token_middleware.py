ea26556986268e34bd1892b3e1566400bb1a87da
Gabriel Gonzalez <gajosu15@gmail.com>
2023-05-24 23:43:15 +0000

    add middleware tests

---
+++ tests/middleware/test_access_token_middleware.py
@@ -0,0 +1,27 @@
+import json
+from unittest import mock
+
+from django.test import Client, RequestFactory, TestCase, override_settings
+
+from chatgpt.views import *
+
+
+class TestAccessTokenMiddlewares(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    @mock.patch('chatgpt.views.Chatbot')
+    def test_ask_view_missing_access_token(self, mock_chatbot):
+        """
+        Test that the access token middleware returns a 401 response if the access token is missing
+        """
+        with override_settings(MIDDLEWARE=['chatgpt.middleware.access_token.AccessTokenMiddleware']):
+            client = Client()
+
+            response = client.post('/conversations')
+
+            self.assertEqual(response.status_code, 401)
+            data = json.loads(response.content)
+            self.assertEqual(data['error'], "access_token is required")
+
+            mock_chatbot.assert_not_called()
---
+++ tests/middleware/test_error_handler_middleware.py
@@ -0,0 +1,43 @@
+import json
+from unittest import mock
+
+from django.test import Client, TestCase, override_settings
+from OpenAIAuth import Error as AuthError
+from revChatGPT.V1 import Chatbot
+
+from chatgpt.views import *
+
+
+class TestErrorHandlerMiddleware(TestCase):
+
+    @mock.patch('chatgpt.views.Chatbot')
+    def test_open_ai_error_handler(self, mock_chatbot):
+        """
+        Test that the error handler middleware catches OpenAIAuth errors and returns a 500 response
+        """
+        with override_settings(MIDDLEWARE=['chatgpt.middleware.error_handler.ErrorHandlerMiddleware']):
+            client = Client()
+
+            mock_chatbot_instance = mock.Mock(spec=Chatbot)
+            mock_chatbot.return_value = mock_chatbot_instance
+
+            mock_chatbot_instance.ask.side_effect = AuthError(
+                location="location",
+                status_code=500,
+                details="details"
+            )
+
+            response = client.post(
+                path='/conversations',
+                data={'prompt': 'Hello'},
+            )
+
+            self.assertEqual(response.status_code, 500)
+
+            data = json.loads(response.content)
+
+            self.assertEqual(data['error'], {
+                "location": "location",
+                "status_code": 500,
+                "details": "details"
+            })

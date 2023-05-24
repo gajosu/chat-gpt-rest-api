3fbf9186aaa6b2052a45299e680cff4bdcdc2ea5
Gabriel Gonzalez <gajosu15@gmail.com>
2023-05-24 05:47:04 +0000

    add environ

--- chatgpt/settings.py
+++ chatgpt/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z(=+ruji#@!&a3=^#(x&9tv2f*y__dl!zwa$b^j47&h4#vrom9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=True)
 
 ALLOWED_HOSTS = []
 
